3b139b28f0f851d821c9171520c1de751ee37b59
Faruk <faruk@Faruk-Air.bbrouter>
2025-11-02 03:14:33 +0300

    Destructive push of all files

--- resources/views/groups/generate/form.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="{{ route('participants.import') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="file" name="file" required>
-    <button type="submit">Import</button>
-</form>
